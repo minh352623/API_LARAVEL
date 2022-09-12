7296b6249cc53b1e01636cf7bdfe052c9df2a1c6
minh <minh@example.com>
2022-09-12 21:54:25 +0700

    ádsa

--- database/migrations/2022_09_04_163048_add_column_user_table.php
+++ database/migrations/2022_09_04_163048_add_column_user_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             //
             $table->string('address')->nullable();
-            $table->string('phone', 10);
+            $table->string('phone', 10)->nullable();
         });
     }
 
