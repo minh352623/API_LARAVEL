0d99857f825ca6ec6111d952a12f9fa8e577265f
minh <minh@example.com>
2022-09-13 12:34:31 +0700

    asdas

--- database/migrations/2022_09_13_123412_add_column_carts2_cart_table.php
+++ database/migrations/2022_09_13_123412_add_column_carts2_cart_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('carts', function (Blueprint $table) {
             //
-            $table->dropColumn('carts');
+            $table->text('carts')->nullable();
         });
     }
 
